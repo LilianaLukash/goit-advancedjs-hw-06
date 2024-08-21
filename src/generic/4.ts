ad1e6dbb1122d128a5be424456fe5194f593b411
Liliana <liliana.ubisoft@gmail.com>
2024-08-21 18:09:15 +0100

    all

--- src/generic/4.ts
+++ src/generic/4.ts
@@ -1,15 +1,17 @@
 /*
   Використовуйте generics та інтерфейси, щоб виправити помилку в наступних класах:
 */
-
-class Component {
-  constructor (public props:T) {
-
-  }
+interface ComponentProps {
+  title: string;
+  [key: string]: any; // Додаткові властивості, якщо вони є
 }
 
-class Page extends Component {
-  pageInfo () {
+class Component<T extends ComponentProps> {
+  constructor(public props: T) {}
+}
+
+class Page extends Component<ComponentProps> {
+  pageInfo() {
     console.log(this.props.title);
   }
 }
